a40d79cc44294bf530c6512ef53659f50346aed0
JesusMezaOrozco <jesus.orozco@3dves.com>
2023-01-08 16:49:01 -0500

    Add: basic ui structure of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { ImQuotesLeft } from 'react-icons/im'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const getNewQuote = () => {}
+
+	return (
+		<div className='vw-100 vh-100 d-flex align-items-center justify-content-center flex-column'>
+			<div id='quote-box' className='quoteContainer'>
+				<div id='text'>
+					<p className='text-center'>
+						<ImQuotesLeft className='quoteIcon' />
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+						minima dolorem ex. Accusantium, quibusdam expli
+					</p>
+				</div>
+				<div id='author' className='d-flex justify-content-end'>
+					<span className='fst-italic' style={{ fontSize: 12 }}>
+						- Mark Twain
+					</span>
+				</div>
+				<div className='d-flex justify-content-between mt-2'>
+					<a className='btn' href='http://www.google.com' id='tweet-quote'>
+						<AiOutlineTwitter />
+					</a>
+					<button id='new' className='btn' onClick={getNewQuote}>
+						New Quote
+					</button>
+				</div>
+			</div>
+			<div>
+				<p style={{ color: 'white', fontSize: 10 }}>by Jesus Meza</p>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
