4a6d5f99aceb3fd1ae0032e6a63640a288ee7105
JesusMezaOrozco <jesus.orozco@3dves.com>
2023-01-08 18:56:26 -0500

    Add: quotes api service

--- src/App.js
+++ src/App.js
@@ -1,37 +1,72 @@
+import { useEffect, useState, useCallback } from 'react'
 import { AiOutlineTwitter } from 'react-icons/ai'
 import { ImQuotesLeft } from 'react-icons/im'
+import axios from 'axios'
 import './App.css'
 
+const apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=happiness'
+const apiKey = 'EuIYu1IzkDhyQbB7Il0ftQ==EUtdRlj78A6xD4ca'
+
 function App() {
-	const getNewQuote = () => {}
+	const [quote, setQuote] = useState({
+		quote: '',
+		author: '',
+	})
+
+	const getNewQuote = async () => {
+		const { data } = await axios.get(apiUrl, {
+			headers: {
+				'X-Api-Key': apiKey,
+			},
+		})
+		setQuote({
+			quote: data[0].quote,
+			author: data[0].author,
+		})
+	}
 
 	return (
 		<div className='vw-100 vh-100 d-flex align-items-center justify-content-center flex-column'>
-			<div id='quote-box' className='quoteContainer'>
-				<div id='text'>
-					<p className='text-center'>
-						<ImQuotesLeft className='quoteIcon' />
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-						minima dolorem ex. Accusantium, quibusdam expli
-					</p>
-				</div>
-				<div id='author' className='d-flex justify-content-end'>
-					<span className='fst-italic' style={{ fontSize: 12 }}>
-						- Mark Twain
-					</span>
-				</div>
-				<div className='d-flex justify-content-between mt-2'>
-					<a className='btn' href='http://www.google.com' id='tweet-quote'>
-						<AiOutlineTwitter />
-					</a>
-					<button id='new' className='btn' onClick={getNewQuote}>
-						New Quote
-					</button>
-				</div>
-			</div>
-			<div>
-				<p style={{ color: 'white', fontSize: 10 }}>by Jesus Meza</p>
-			</div>
+			{quote.quote ? (
+				<>
+					<div id='quote-box' className='quoteContainer'>
+						<div id='text'>
+							<p className='text-center'>
+								<ImQuotesLeft className='quoteIcon' />
+								{quote.quote}
+							</p>
+						</div>
+						<div id='author' className='d-flex justify-content-end'>
+							<span className='fst-italic' style={{ fontSize: 12 }}>
+								- {quote.author}
+							</span>
+						</div>
+						<div className='d-flex justify-content-between mt-2'>
+							<a
+								className='btn'
+								href='http://www.twitter.com/intent/tweet'
+								id='tweet-quote'
+							>
+								<AiOutlineTwitter />
+							</a>
+							<button id='new-quote' className='btn' onClick={getNewQuote}>
+								New Quote
+							</button>
+						</div>
+					</div>
+					<div>
+						<p style={{ color: 'white', fontSize: 10 }}>by Jesus Meza</p>
+					</div>
+				</>
+			) : (
+				<button
+					id='new-quote'
+					className='btn btn-primary'
+					onClick={getNewQuote}
+				>
+					Get Quote
+				</button>
+			)}
 		</div>
 	)
 }
